9821baa360c45ae46abb29b5267f45837c1a0220
Woojeong <alsxmakt123@naver.com>
2026-05-28 14:52:25 +0200

    ex00 en cours

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -2,5 +2,24 @@
 
 int main(void)
 {
-    
+	std::cout << "############### Create vector container of int {2, 12, 97, 5}###############" << std::endl << std::endl;
+	std::vector<int>	vec;
+	vec.push_back(2);
+	vec.push_back(12);
+	vec.push_back(97);
+	vec.push_back(5);
+
+	std::cout << "############### TEST: search 12 ###############" << std::endl << std::endl;
+	try
+	{
+		int	i = 12;
+		std::vector<int>::const_iterator	it;
+		it = easyfind(std::vector<type>, i);
+		std::cout << "result: " << it << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "not found" << std::endl;
+	}
+	return (0);
 }
